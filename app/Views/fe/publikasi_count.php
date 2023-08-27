a64a36d72644f14d1ed52325e824ae7f27454f50
yusufye <email.yusufye@gmail.com>
2023-08-27 18:04:12 +0700

    fix kebijakan diklat count

--- app/Views/fe/publikasi_count.php
+++ app/Views/fe/publikasi_count.php
@@ -7,17 +7,18 @@
 
         <div class="row">
 
-            <div class="col-lg-6 col-md-6 col-sm-6" data-aos="zoom-in" data-aos-delay="100">
-                <div class="icon-box">
-                    <div class="icon"><i class='bx bxs-video'></i></div>
-                    <h4><a href="">Jumlah Video</a></h4>
-                    <p>
-                    <h5><span data-purecounter-start="0"
-                            data-purecounter-end="<?php echo $count[$model_name]['video'][0]['total']; ?>"
-                            data-purecounter-duration="1" class="purecounter"></span> Data</h5>
-                    </p>
-                    <a href="<?php echo base_url()."/publikasi/$orig_title/video/detail" ?>" class="btn btn-warning"><i
-                            class='bx bx-right-arrow-circle'></i> Info Detail</i></a>
+                <div class="col-lg-6 col-md-6 col-sm-6" data-aos="zoom-in" data-aos-delay="100">
+                    <div class="icon-box">
+                        <div class="icon"><i class='bx bxs-video'></i></div>
+                        <h4><a href="">Jumlah Video</a></h4>
+                        <p>
+                        <h5><span data-purecounter-start="0"
+                                data-purecounter-end="<?php echo $count[$model_name]['video'][0]['total']; ?>"
+                                data-purecounter-duration="1" class="purecounter"></span> Data</h5>
+                        </p>
+                        <a href="<?php echo base_url()."/publikasi/$orig_title/video/detail" ?>" class="btn btn-warning"><i
+                                class='bx bx-right-arrow-circle'></i> Info Detail</i></a>
+                    </div>
                 </div>
 
                 <div class="col-lg-6 col-md-6 col-sm-6" data-aos="zoom-in" data-aos-delay="200">
@@ -31,6 +32,7 @@
                         </p>
                         <a href="<?php echo base_url()."/publikasi/$orig_title/paparan/detail" ?>"
                             class="btn btn-warning"><i class="bx bx-right-arrow-circle"></i> Info Detail</i></a>
+                    </div>
                     </div>
 
                 </div>
